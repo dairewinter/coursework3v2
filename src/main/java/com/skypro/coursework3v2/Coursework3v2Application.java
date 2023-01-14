c84e3f501b3933ef3e1fb3d92ac9edf9e78785c3
Daria <m-alberts@yandex.ru>
2023-01-14 18:02:37 +0300

    Initial commit

---
+++ src/main/java/com/skypro/coursework3v2/Coursework3v2Application.java
@@ -0,0 +1,13 @@
+package com.skypro.coursework3v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3v2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3v2Application.class, args);
+    }
+
+}
